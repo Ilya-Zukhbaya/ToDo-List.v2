ef2c28ff5278c77a8a41b0a7ad7a63f27eebccfe
Ilya Zukhbaya <ne_perepletov@mail.ru>
2022-06-09 00:38:11 +0300

    Demo ToDo.v2 is done

--- src/data/stores/useToDostore.ts
+++ src/data/stores/useToDostore.ts
@@ -15,7 +15,18 @@ interface ToDoStore {
 }
 
 export const useToDoStore = create<ToDoStore>((set, get) => ({
-    tasks: [],
+    tasks: [
+        {
+            id: 'ADFAFF',
+            title: 'Task 1',
+            createdAt: 132324
+        },
+        {
+            id: 'ADFAffFF',
+            title: 'Task 2',
+            createdAt: 32324
+        }
+    ],
     createTask: (title) => {
         const { tasks } = get()
         const newTasks = {
@@ -28,6 +39,19 @@ export const useToDoStore = create<ToDoStore>((set, get) => ({
             tasks: [newTasks].concat(tasks)
         })
     },
-    updateTask: (title, id) => {},
-    removeTask: (id) => {}
+    updateTask: (id: string, title: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.map((task) => ({
+                ...task,
+                title: task.id === id ? title : task.title,
+            }))
+        });
+    },
+    removeTask: (id: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.filter((task) => task.id !== id)
+        })
+    }
 }))
--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./index.module.scss";
 import { useToDoStore } from '../../data/stores/useToDostore'
-
+import { InputPlus } from "../components/InputPlus"
+import { InputTask } from "../components/InputTask";
 export const App: React.FC = () => {
     const [tasks,
         createTask,
@@ -13,14 +14,31 @@ export const App: React.FC = () => {
         state.updateTask,
         state.removeTask
     ])
+
     return (
         <article className={styles.main}>
-            <h1 className={styles.Title}>ToDo App</h1>
-            <section className={styles.main__firstSection}>
-
+            <h1 className={styles.mainTitle}>ToDo App</h1>
+            <section className={styles.mainFirstSection}>
+                <InputPlus onAdd={(title) => {
+                    if (title) {
+                        createTask(title)
+                    }
+                }} />
             </section>
-            <section className={styles.main__secondSection}>
-
+            <section className={styles.mainSecondSection}>
+                {!tasks.length && (
+                    <p className={styles.mainNoTaskText}>You have not added any task...</p>
+                )}
+                {tasks.map((task) => (
+                    <InputTask
+                        key={task.id}
+                        id={task.id}
+                        title={task.title}
+                        onDone={removeTask}
+                        onEdited={updateTask}
+                        onRemoved={removeTask}
+                    />
+                ))}
             </section>
         </article>
     )
